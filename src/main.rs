eb584eae794c778ce8b517a01b222e590e68cd14
Marco Russo <nospam@mrusso.de>
2024-05-17 07:16:19 +0000

    Made cargo fmt happy

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,8 @@ async fn health_check(_req: HttpRequest) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-        .route("/health_check", web::get().to(health_check))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
