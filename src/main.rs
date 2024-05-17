0486115bc2aa808062e24fffb87d923e78620449
Marco Russo <nospam@mrusso.de>
2024-05-17 06:29:43 +0000

    Web-Helloworld

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,18 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            //.service(hello)
+            //.service(echo)
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
